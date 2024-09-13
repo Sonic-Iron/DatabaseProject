f0a9d8f9a92057ac68b10992c73ac64ad8215e0c
Sonic-Iron <sam@thehewittfamily.com>
2024-09-13 17:05:54 +0100

    push all updates, small changes

--- TableHeader.h
+++ TableHeader.h
@@ -1,5 +1,9 @@
 #pragma once
 
+//data types:
+// numbers?
+// words?
+// file types? and a reference to the file? (a directory path?)
 
 class Table : Database
 {
@@ -7,8 +11,8 @@ private:
 
 public:
 	Table(std::string PKname);
+
 	using Database::GetDirectory;
-	
 	std::string GetDirectory(Table& table);
 	
 	static Table CreateTable(std::vector<std::string> entrytypes);
